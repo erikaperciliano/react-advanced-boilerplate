506042f42b6941ce1b69747b3f3cabf31c59e657
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-08-17 21:23:00 -0300

    Updating first Page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,19 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Advanced React</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image of an atom and Advanced React written on the side."
+    />
+    <S.Title>Advanced React</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer in front of the screen with code"
+    />
+  </S.Wrapper>
 )
 export default Main
