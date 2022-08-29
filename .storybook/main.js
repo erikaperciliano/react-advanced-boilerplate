d06e18a11396743c59d1e5cdadfd018402313fb7
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-08-29 17:22:14 -0300

    add the path of the file that will be watched by the storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/components/**/*stories.tsx"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-knobs"
+    "@storybook/addon-knobs",
+    "@storybook/preset-typescript"
   ],
   "framework": "@storybook/react",
   "core": {
