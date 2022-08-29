da9a46a57ab5e67dd82e036e0245e51046e8779b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-08-29 19:25:30 -0300

    using the decorator inside of the Main Component  passing the default values

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,8 +1,18 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  decorators: [withKnobs]
 }
 
-export const Basic = () => <Main />
+export const Basic = () => (
+  <Main
+    title={text('Title', 'Advanced React')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS and Styled Components'
+    )}
+  />
+)
