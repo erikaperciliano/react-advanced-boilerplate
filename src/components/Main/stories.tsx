eecb0f2124ad2a53d43980e7fb115eb6dfb3d725
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-08-30 20:31:53 -0300

    add the Basic and Default stories for this specific Story

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Testing1',
+  description: 'Testing2'
 }
 
-export const Basic = () => (
-  <Main
-    title={text('Title', 'Advanced React')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS and Styled Components'
-    )}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: 'Hello World!',
+  description: 'Talk Dev!'
+}
