36c3bae9b99e4993cf1b4ca992624a0794c34182
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-08-30 20:36:33 -0300

    pass the value default for all stories

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -3,17 +3,13 @@ import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
-Basic.args = {
-  title: 'Testing1',
-  description: 'Testing2'
-}
 
 export const Default: Story = (args) => <Main {...args} />
-Default.args = {
-  title: 'Hello World!',
-  description: 'Talk Dev!'
-}
