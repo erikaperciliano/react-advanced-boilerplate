3d8423bd72a60ba5faf77dd464e22cbd42b0cb47
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-09-01 22:38:29 -0300

    add the manifest inside of the app.js file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title> Advanced React - Boilerplate</title>
         <link rel="shotcut icon" href="/img/favicon.ico" />
         <link rel="apple-touch-icon" href="/img/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
